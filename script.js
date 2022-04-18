c2b9db24f1769db0196b9c4f0197dd74820eb691
BabsD <aboubacar.diaw17@gmail.com>
2022-04-18 14:29:35 +0200

    update for assignment

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    console.log("In compute");
+    let p = document.getElementById("principal").value;
+    if(p === "") {
+        alert("Enter the amount");
+        return;
+    }
+    else if (p <= 0)
+    {
+        alert("Enter a positive number");
+        return;
+
+    }
+    let rate = document.getElementById("rate").value;
+    let numberOfyears = document.getElementById("years").value;
+    let endYear = new Date().getFullYear()+parseInt(numberOfyears);
+
+    let gain = (p*rate *numberOfyears)/100;
+
+    let interest = principal * numberOfyears * rate /100;
+    document.getElementById("amount_value").textContent = p;
+    document.getElementById("interest_value").textContent = rate;
+    document.getElementById("received_amount").textContent = gain;
+    document.getElementById("year_value").textContent = endYear;
+    document.getElementById("result_div").classList.remove("invisible");
+
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_value").innerText=rateval +"%";
+}
+
+function checkPrincipalInput(value) {
+    if(value <= 0 || value === "" ) {
+        alert("Enter a positive number");
+    }
+}
